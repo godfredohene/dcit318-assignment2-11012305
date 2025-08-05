e6ed91d7bdd23d62b46d4b58f554831e1459279c
godfred michael ohene <gmohene@st.ug.edu.gh>
2025-08-05 21:15:20 +0000

    Add InterfacesApp demonstrating interfaces in C#

---
+++ InterfacesApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterfacesApp
+{
+    // Define an interface
+    interface IAnimal
+    {
+        void MakeSound();
+    }
+
+    // Implement the interface
+    class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Cat says: Meow! Meow!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal myCat = new Cat();
+            myCat.MakeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
